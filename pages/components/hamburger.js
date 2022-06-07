4190d2c7e39da03711c9a0ed2e911464e5af3a20
hadis soleymani <hadissoleymani.it@gmail.com>
2022-06-07 12:28:13 -0700

    tablet and mobile navbar

--- pages/components/hamburger.js
+++ pages/components/hamburger.js
@@ -1,52 +1,53 @@
-import React, { Component } from 'react'
-import styled from 'styled-components'
-import Navbar from './navbar'
+import React, { Component } from "react";
+import styled from "styled-components";
+import Navbar from "./navbar";
 
 const Div = styled.div`
-width:2rem;
-height: 2rem;
-position:fixed;
-top:2vh;
-left:5vw;
-z-index:20;
-display:none;
-cursor: pointer;
+  width: 2rem;
+  height: 2rem;
+  position: fixed;
+  top: 2vh;
+  left: 5vw;
+  z-index: 20;
+  display: none;
+  cursor: pointer;
 
-@media(max-width: 768px){
-  display:flex;
-  justify-content: space-around;
-  flex-direction: column;
-}
-div{
-  height:0.25rem;
-  width:2rem;
-  background:${props => props.open ?  '#42474b' :'rgb(234, 233, 239)' };
-  border-radius:10px;
-  transform-origin:1px;
-  transition: all 0.3s linear;
-  &:nth-child(1){
-    transform:${props => props.open ? 'rotate(45deg)' : 'rotate(0)'};
+  @media (max-width: 1200px) and (min-width: 300px) {
+    display: flex;
+    justify-content: space-around;
+    flex-direction: column;
   }
-  &:nth-child(2){
-    transform:${props => props.open ? 'translateX(-100%)' : 'translateX(0)'};
-    opacity:${props => props.open ? 0 : 1};
+  div {
+    height: 0.25rem;
+    width: 2rem;
+    background: ${(props) => (props.open ? "#42474b" : "rgb(234, 233, 239)")};
+    border-radius: 10px;
+    transform-origin: 1px;
+    transition: all 0.3s linear;
+    &:nth-child(1) {
+      transform: ${(props) => (props.open ? "rotate(45deg)" : "rotate(0)")};
+    }
+    &:nth-child(2) {
+      transform: ${(props) =>
+        props.open ? "translateX(-100%)" : "translateX(0)"};
+      opacity: ${(props) => (props.open ? 0 : 1)};
+    }
+    &:nth-child(3) {
+      transform: ${(props) => (props.open ? "rotate(-45deg)" : "rotate(0)")};
+    }
   }
-  &:nth-child(3){
-    transform:${props => props.open ? 'rotate(-45deg)' : 'rotate(0)'}
-  }
-}
-`
+`;
 
 export default class Hamburger extends Component {
   constructor() {
     super();
     this.state = {
-      open: false
-    }
+      open: false,
+    };
   }
   openHandler = () => {
-    this.setState({ open: !this.state.open })
-  }
+    this.setState({ open: !this.state.open });
+  };
   render() {
     return (
       <>
@@ -54,10 +55,9 @@ export default class Hamburger extends Component {
           <div></div>
           <div></div>
           <div></div>
-
         </Div>
         <Navbar open={this.state.open} />
       </>
-    )
+    );
   }
 }
--- pages/components/navbar.js
+++ pages/components/navbar.js
@@ -2,42 +2,44 @@ import styles from "../../styles/navbar.module.css";
 import styled from "styled-components";
 
 const Ul = styled.ul`
-    list-style:none;
-    display:flex;
-    justify-content: right;
-    z-index:15;
-    li:hover{
-      color:rgb(226, 224, 74);
-      transition:all .2s ease-in;
-    }
-    li{
-        padding:2vw;
-        font-size:1.3rem;
-        font-weight:600;
-    }
-    @media (max-width: 768px){
-        flex-direction:column;
-        background-color: #fff;
-        margin:0;
-        color:#42474b;
-        position:fixed;
-        box-shadow: 3px 3px 10px #cbced1;
-        transition: all 0.3s linear;
-        transform: ${props => props.open ? 'translateX(0)':'translateX(-100%)' };
-        left:0;
-        top:0;
-        padding-top:8vh;
-        width:65vw;
-        height: 100vh;
+  list-style: none;
+  display: flex;
+  justify-content: right;
+  z-index: 15;
+  li:hover {
+    color: rgb(226, 224, 74);
+    transition: all 0.2s ease-in;
+    cursor:pointer;
+  }
+  li {
+    padding: 1.2vw;
+    font-size: 1.3rem;
+    font-weight: 600;
+  }
+  @media (max-width: 1200px) and (min-width: 300px) {
+    flex-direction: column;
+    background-color: #fff;
+    margin: 0;
+    color: #42474b;
+    position: fixed;
+    box-shadow: 3px 3px 10px #cbced1;
+    transition: all 0.3s linear;
+    transform: ${(props) =>
+      props.open ? "translateX(0)" : "translateX(-100%)"};
+    left: 0;
+    top: 0;
+    padding-top: 8vh;
+    width: 65vw;
+    height: 100vh;
+    
+  }
+`;
 
-    }
-`
-
-export default function Navbar({open}) {
+export default function Navbar({ open }) {
   return (
     <div className={styles.container}>
       <h1>Hadis Soleymani</h1>
-      <Ul open={open} >
+      <Ul open={open}>
         <li>Home</li>
         <li>About me</li>
         <li>Services</li>
