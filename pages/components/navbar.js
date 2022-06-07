8cc440bd5dcc9d6e9c80356f2ce535b62b76e0e4
hadis soleymani <hadissoleymani.it@gmail.com>
2022-06-07 11:43:09 -0700

    ul li

--- pages/components/navbar.js
+++ pages/components/navbar.js
@@ -4,16 +4,16 @@ export default function Navbar() {
   return (
     <div className={styles.container}>
       <h1>حدیث سلیمانی</h1>
-      <div className={styles.item_container}>
-        <p>خانه</p>
-        <p>درباره ی من</p>
-        <p>خدمات</p>
-        <p>تجربه ها</p>
-        <p>اعضای تیم</p>
-        <p>وبلاگ</p>
-        <p>ارتباط با من</p>
-        <p>نمونه کار ها</p>
-      </div>
+      <ul className={styles.item_container}>
+        <li>خانه</li>
+        <li>درباره ی من</li>
+        <li>خدمات</li>
+        <li>تجربه ها</li>
+        <li>اعضای تیم</li>
+        <li>وبلاگ</li>
+        <li>ارتباط با من</li>
+        <li>نمونه کار ها</li>
+      </ul>
     </div>
   );
 }
