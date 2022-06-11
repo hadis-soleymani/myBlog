9df2405a50aae7fc4ea2c3d58bd1fe5012706b4b
hadis soleymani <hadissoleymani.it@gmail.com>
2022-06-11 08:49:00 -0700

    add works link

--- pages/work_card.js
+++ pages/work_card.js
@@ -1,19 +1,37 @@
 import React from "react";
-import Image from 'next/image'
+import Image from "next/image";
+import Link from "next/link";
 
 //styles
 import styles from "../styles/works.module.css";
 
-const Work_card = ({ image, name }) => {
+const Work_card = ({ image, name, video, git, download }) => {
   return (
     <div className={styles.card_container}>
-      <Image
-        src={`/${image}`}
-        alt="avatar"
-        width={140}
-        height={140}
-      />
+      <Image src={`/${image}`} alt="avatar" width={140} height={140} />
       <h2>{name}</h2>
+
+      {git != null && (
+        <Link href={`${git}`}>
+          <a target="_blank" rel="noreferrer">
+            Github Code
+          </a>
+        </Link>
+      )}
+      {download != null && (
+        <Link href={`${download}`}>
+          <a target="_blank" rel="noreferrer">
+            Download link
+          </a>
+        </Link>
+      )}
+      {video != null && (
+        <Link href={`${video}`}>
+          <a target="_blank" rel="noreferrer">
+            Download Demo Video
+          </a>
+        </Link>
+      )}
     </div>
   );
 };
--- pages/works.js
+++ pages/works.js
@@ -17,17 +17,42 @@ const Works = () => {
     <div className={styles.works_container} id="works">
       <h1>Works</h1>
       <div className={styles.cards_container}>
-        <Work_card image={"plant.svg"} name="Nahalito" />
-        <Work_card image={"online-shop.svg"} name="Online Shop" />
-        <Work_card image={"wallet.svg"} name="Smart City" />
+        <Work_card
+          image={"plant.svg"}
+          name="Nahalito"
+          download="https://cafebazaar.ir/app/com.nahalito"
+          video="https://drive.google.com/file/d/1CDIVUGcOu4sYOgrC5XXT0ctMS0bT7mSS/view?usp=sharing"
+        />
+        <Work_card
+          git="https://github.com/hadis-soleymani/online-shop"
+          video="https://drive.google.com/file/d/1nEJrapesXABVkn5qHzOQKotRO0Nwx9Qw/view?usp=sharing"
+          image={"online-shop.svg"}
+          name="Online Shop"
+        />
+        <Work_card
+          download="https://cafebazaar.ir/app/com.scanqrcode"
+          video="https://drive.google.com/file/d/1oWtT4Krg-OgnAaAjqzkAikV1EYPJY87z/view?usp=sharing"
+          image={"wallet.svg"}
+          name="Smart City"
+        />
         {show ? (
           <>
             <Work_card
               image={"wallet-management.svg"}
               name="Wallet Management"
+              video="https://drive.google.com/file/d/1IuMEVaYsVfla1yabOlhukdx-gUi0F9lm/view?usp=sharing"
             />
-            <Work_card image={"chat.svg"} name="Automation" />
-            <Work_card image={"login.svg"} name="Authentication test" />
+            <Work_card
+              image={"chat.svg"}
+              video=" https://drive.google.com/file/d/1yTTDOzcG6RhjeuI3o1DT0gyIJZ-c19Uz/view?usp=sharing"
+              name="Automation"
+            />
+            <Work_card
+              image={"login.svg"}
+              git="https://github.com/hadis-soleymani/signin"
+              video="https://drive.google.com/file/d/12FUFfckU1RUU-IvEAT5e1rbn0RaIok05/view?usp=sharing"
+              name="Authentication test"
+            />
           </>
         ) : null}
       </div>
