220b16308819994c5012253e9671fdfe1144c3a6
hadis soleymani <hadissoleymani.it@gmail.com>
2022-06-11 04:54:43 -0700

    change name of class

--- pages/home.js
+++ pages/home.js
@@ -1,5 +1,5 @@
 //styles
-import styles from '../styles/home.module.css';
+import styles from '../styles/home_sec.module.css';
                           
 //Icons
 import { FaInstagram, FaWhatsapp } from "react-icons/fa";
