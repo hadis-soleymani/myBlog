6e4f5e9cd18df30630f3913ee55ecc4e86214da8
hadis soleymani <hadissoleymani.it@gmail.com>
2022-07-12 06:11:26 -0700

    add ccomment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
+  
   reactStrictMode: true,
+  //for external images must add domain
   images: {
     domains: ['res.cloudinary.com'],
   },
