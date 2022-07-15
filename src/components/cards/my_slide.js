e9692c21b3843cb6ce69c4da2f2b7c35991a2378
hadis soleymani <hadissoleymani.it@gmail.com>
2022-07-15 08:54:28 -0700

    change key

--- src/components/cards/my_slide.js
+++ src/components/cards/my_slide.js
@@ -1,28 +1,23 @@
-import React from 'react'
-import styles from './my_slide.module.css'
+import React from "react";
+import styles from "./my_slide.module.css";
 import Image from "next/image";
-import Slider from "react-styled-carousel"
+import Slider from "react-styled-carousel";
 
-const My_slide = ({data}) => {
+const My_slide = ({ data }) => {
   return (
     <Slider
-    className={styles.sliders_container}
-    cardsToShow={1}
-    pauseOnMouseOver={true}
-    infinite={false}
-  >
-    {data.pic.map((item) => (
-      <div className={styles.image_container}>
-      <Image
-        src={item.slide_pic_url}
-        width={item.width}
-        key={item.id}
-        height={300}
-      />
-      </div>
-    ))}
-  </Slider>
-  )
-}
+      className={styles.sliders_container}
+      cardsToShow={1}
+      pauseOnMouseOver={true}
+      infinite={false}
+    >
+      {data.pic.map((item) => (
+        <div className={styles.image_container} key={item.id}>
+          <Image src={item.slide_pic_url} width={item.width} height={300} />
+        </div>
+      ))}
+    </Slider>
+  );
+};
 
-export default My_slide
+export default My_slide;
